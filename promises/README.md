ed18399fa40b7bbef674bdef77d8f282f5280362
Moises Montenegro <mmontenegro@shoprunner.com>
2018-09-17 16:55:23 -0500

    Updated promises example

---
+++ promises/README.md
@@ -0,0 +1,10 @@
+# Promises
+
+## What are promises?
+-  Promises are objects that have success and failure callbacks attached. This is done instead of passing in success and failure function as callbacks into an enclosing function
+
+1. Executor function
+  - A function that is passed in with the resolve and reject, and is invoked immediately in the Promise constructor
+
+## States and Fates
+- https://github.com/domenic/promises-unwrapping/blob/master/docs/states-and-fates.md
--- promises/js/index.js
+++ promises/js/index.js
@@ -1,19 +1,61 @@
 (function() {
+
+  // returns 1 or 0 with floored input of 2
+  let randInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   let pinkyPromise = () => {
 
     return new Promise( (resolve, reject) => {
-      console.log('hello');
-      
-      // successful call to api
-      setTimeout(() => resolve("done!"), 1000);
-    });
+      // resolve and reject are built-in Javascript functions for the Promise
+      // constructor
+      console.log('Promise executor context, invoked immediately as new Promise is constructed');
 
+      if(randInt(2) === 1) {
+        // on success, invoke resolve with parameters as return to .then() chain
+        setTimeout(() => resolve("done!"), 1000);
+      } else {
+        // on failure, invoke reject with parameters as return to .then() chain
+        setTimeout(() => reject( Error("failed")), 1000) ;
+      }
+    });
   }
 
   let moePromise = pinkyPromise();
 
-  moePromise.then( () => {
-    console.log('then promise');
-  });
+  // Can't really log Promise, it is a promise, and must be .then() to actually view
+  // its contents.
 
+  // Contains
+  // 1. Pending
+  // 2. Fulfilled
+  // 3. Rejected
+  console.log(moePromise);
+
+
+  moePromise.then( (result) => {
+    
+    console.log('first then');
+    
+    // Will run after subsquent then below
+    // return setTimeout(() => {
+    // console.log('Timeout from inner timeout')
+    // });
+
+    // Creates a new promise, that is returns a promise that serves as a proxy until the inner executor, created by
+    // the Promise
+    return new Promise( (resolve, reject) => {
+      setTimeout( () => {
+        resolve('inner promise from first then, after 2 seconds')
+      }, 2000);
+    });
+
+  }).then((result) => {
+    console.log(`${result}`)
+    console.log('second then')
+  }).catch((error) => {
+    console.log(error, ' was caught');
+    return false;
+  });
 })();
