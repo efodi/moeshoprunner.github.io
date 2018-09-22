24bf9cc8e461de639d0422a627a297f81fe5a01a
Moises Montenegro <mmontenegro@shoprunner.com>
2018-09-22 08:34:30 -0500

    Updated example

--- promises/js/await.js
+++ promises/js/await.js
@@ -1,15 +1,25 @@
 const pinkyPromise = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('Moe is on Beertation!')
-        }, 2000);
+            // resolve('Moe is on Beertation!');
+        }, 500);
     });
 };
 
 
-const makeRequest = async = () => {
-    await pinkyPromise();
+const makeRequest = async() => {
+    // console.log(await pinkyPromise());
     return "Success";
 };
 
-makeRequest();
+// Implicitly returns a promise with a value of the return
+// So would be similar to
+
+/*
+    return new Promise((resolve, reject) => {
+        resolve("Success");
+    });
+*/
+
+const result = makeRequest();
+console.log(result);
