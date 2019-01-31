2f1c5f5555fdb565f169a4daebf7c03adad0a143
Moises Montenegro <mmontenegro@shoprunner.com>
2019-01-31 10:47:56 -0600

    Added test folder

--- algorithms/js/recursion.js
+++ algorithms/js/recursion.js
@@ -19,9 +19,44 @@
         return n * factorial(n - 1);
     };
 
+    // const fibonacci = (n) => {
+    //     // returns the number itself and the number before it
+    //
+    //     //base case
+    //     if (n <= 2) return 1;
+    //
+    //     while (n < 100) {
+    //         return n + fibonacci(n - 1);
+    //     }
+    // };
+
+    const fibonacci = num =>  {
+      if (num <= 1) return 1;
+      // if (num >= 20) {
+      //     console.log("catch a crash!");
+      //     return false;
+      // }
+
+      // const guard = fibonacci(num - 1) + fibonacci(num -2);
+      //
+      // console.log(guard);
+      //
+      // if(guard > 1000) {
+      //    return guard;
+      // }
+
+      // Way too expensive, use memoization
+      // https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+      return fibonacci(num - 1) + fibonacci(num - 2);
+    };
+
+
     document.addEventListener("DOMContentLoaded", (event) => {
         const resultDom = document.getElementById('result');
-        const result = factorial(10);
-        resultDom.innerHTML = result;
+        // const result = factorial(10);
+        const result = fibonacci(5);
+
+        resultDom.append(result);
+        // console.log(fibonacci(2));
     });
 })();
