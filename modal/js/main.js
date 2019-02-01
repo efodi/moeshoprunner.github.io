0a93388dcfe78221d5d28f0436030ee81e78b143
Moises Montenegro <mmontenegro@shoprunner.com>
2019-02-01 09:52:53 -0600

    Updated iframe accessible approach

--- modal/js/main.js
+++ modal/js/main.js
@@ -4,10 +4,28 @@
     MicroModal.init();
 
     var $modalButton = document.getElementById('show-modal');
+    var iframe = document.querySelector('.modal iframe');
+    var modalOpen = false;
+    var $closeButton;
 
     $modalButton.addEventListener('click', function(event) {
-      var iframe = document.querySelector('.modal iframe');
+      modalOpen = true;
+      $closeButton = document.querySelector('.modal__close')
       iframe.contentWindow.focus();
+    });
+
+    document.addEventListener('focusin', function(event) {
+      if(modalOpen && event.target === $modalButton) {
+        console.log(event.target, ' is the focused in element');
+        $closeButton.focus();
+      }
+    });
+
+    document.addEventListener('focusout', function(event) {
+      if(modalOpen && event.target === $modalButton) {
+        console.log(event.target, ' is the focused out element');
+        $closeButton.focus();
+      }
     });
   });
 
